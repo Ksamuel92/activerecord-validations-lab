67c5ba8f0b5f451a875b0271a9fbe5b908ed1d23
Kyle Samuel <Ksamuel92@gmail.com>
2021-07-01 11:26:06 -0400

    commit tree 7472c2cb7fc0c840e907c1625fa073a10460640d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates_with CategoryValidator
+    # validates_with ClickbaitValidator
+    validates :title, presence: true
+    validates :content, length: {minimum: 10}
+    validates :summary, length: {maximum: 15}
+    validates :category, presence: true
+    validate :is_clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def is_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
